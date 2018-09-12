2c272cac5252c698b8099d193c42a62854f8624b
Jamest QA Analyst <jamest@ziprecruiter.com>
2018-09-12 15:29:51 -0700

    date back to 2018 exercise1

--- Exercise1.py
+++ Exercise1.py
@@ -1,5 +1,5 @@
 #This will ask for name and age and tell you when you'll be 100. Using raw_input instead of 'input' cuz python 2 uses raw_input. Can run python3 exercise1.py with 'input'
-year = 2020
+year = 2018
 name = raw_input("What is your name?")
 print("Your name is: " + str(name))
 age = int(raw_input("How old are you?"))
