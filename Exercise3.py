652b08cf2bbcc6f43e67add98329e4aae5601293
Jamest QA Analyst <jamest@ziprecruiter.com>
2018-09-11 20:50:14 -0700

    Added 2 files updated 1

---
+++ Exercise3.py
@@ -0,0 +1,23 @@
+#This program will take your inputed number and check the list and return all numbers lower than and equal to inputed number in a new list.
+
+numanswer = int(input("Please enter a number: "))
+
+def newlist(numanswer):
+
+	a = [1,1,2,3,5,8,13,21,34,55,89,98,102,123,144,156,200,232,300]
+	new_list = []
+
+	for element in a:
+		if element <= numanswer:
+			new_list.append(element)
+	print(new_list)
+
+newlist(numanswer)
+
+
+"""for element in a:
+	if element <= 5:
+		new_list.append(element)
+		print(new_list)
+	else:
+		quit()"""
